5d0ea9a4841733404254b8585db01caf5c45b5f8
Aleksey Kozlenkov <akozlenkov@evolution.com>
2021-04-11 13:10:51 +0300

    Add active columns class

--- homeworks/01-bubble-sort/src/array/array.tsx
+++ homeworks/01-bubble-sort/src/array/array.tsx
@@ -1,20 +1,26 @@
+import classNames from 'classnames'
 import { FC } from 'react'
 import { VALUES_RANGE } from '../utils/random'
 import './index.css'
 
 type Props = {
   array: number[]
+  activeIndex: number | null
 }
 
 const maxValue = VALUES_RANGE[1]
 
-const ArrayComponent: FC<Props> = ({ array }) => {
+const isActive = (index: number, activeIndex: number | null): boolean => {
+  return activeIndex != null && (index === activeIndex || index === activeIndex + 1)
+}
+
+const ArrayComponent: FC<Props> = ({ array, activeIndex }) => {
   return (
     <div className="array-container">
       {array.map((value, index) => (
         <div
           key={index}
-          className="element"
+          className={classNames('element', { active: isActive(index, activeIndex) })}
           style={{ height: `${value / maxValue * 100}%` }}
         >
           {value}
