0069e3529a41f75fd4f517ccafbb4bf61b2701ed
akarpenko <akarpenko@evolution.com>
2023-01-25 15:58:17 +0400

    Fix dispatch issue

--- presentations/UnitTesting-v2/react-example/__tests__/ReduxExample.spec.tsx
+++ presentations/UnitTesting-v2/react-example/__tests__/ReduxExample.spec.tsx
@@ -28,25 +28,32 @@ describe("OurCounterReduxComponent", () => {
     })
 
     describe('clicking the button triggers the dispatch and changes the state', () => {
-        const dispatch = jest.fn()
         const store = createStore(counterReducer, { counter: 0 })
+        const dispatch = jest.spyOn(store, "dispatch")
 
         test('clicking the increment button', async () => {
-            const { getByText, debug } = render(
+            const { getByRole, getByText, debug } = render(
                 <Provider store={store}>
                     <OurCounterReduxComponent />
                 </Provider>
             )
 
-            const incrementButton = getByText('+')
+            const incrementButton = getByRole('button', { name: '+' })
+            const decrementButton = getByRole('button', { name: '-' })
             
             fireEvent.click(incrementButton)
             fireEvent.click(incrementButton)
+            fireEvent.click(decrementButton)
             fireEvent.click(incrementButton)
 
-            waitFor(() => expect(dispatch).toHaveBeenCalled())
-            expect(store.getState()).toEqual({ counter: 3 })
-            expect(getByText('Count is 3')).toBeInTheDocument()
+            expect(dispatch).toHaveBeenCalledTimes(4)
+            expect(dispatch).toHaveBeenNthCalledWith(1, { type: 'increment' })
+            expect(dispatch).toHaveBeenNthCalledWith(2, { type: 'increment' })
+            expect(dispatch).toHaveBeenNthCalledWith(3, { type: 'decrement' })
+            expect(dispatch).toHaveBeenNthCalledWith(4, { type: 'increment' })
+
+            expect(store.getState()).toEqual({ counter: 2 })
+            expect(getByText('Count is 2')).toBeInTheDocument()
         })
     })
 })
